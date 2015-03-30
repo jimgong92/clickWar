a2aa28f7d1845cd7462cc63de6d523be796800d4
Jimmy Gong <gong.jim@gmail.com>
2015-03-30 16:53:26 -0700

    Further scaffolds router

--- config/routes.js
+++ config/routes.js
@@ -1,24 +1,46 @@
 var router = function(app, passport){
+  /**
+   * Click Page
+   */
   app.get('/', function(req, res){
     res.render('Hello World');
   });
+  app.post('/', function(req, res){
 
+  });
+  /**
+   * Signup/Login
+   */
   app.get('/signup', function(req, res){
 
   });
+  app.post('/signup', function(req, res){
 
+  });
   app.get('/login', function(req, res){
 
   });
-  app.get('/profile', function(req, res){
+  app.post('/login', function(req, res){
 
   });
+  /**
+   * Protected by auth, must be logged in
+   */
+  app.get('/profile', isLoggedIn, function(req, res){
+
+  });
+  /**
+   * Logout
+   */
   app.get('/logout', function(req, res){
     req.logout();
     res.redirect('/');
   });
 }
 
+/**
+ * Validates login
+ */
 function isLoggedIn(req, res, next){
   if (req.isAuthenticated()){
     return next();
