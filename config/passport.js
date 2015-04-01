1988edaa9dc999867277060e046bf8dd3b70a0d5
Jimmy Gong <gong.jim@gmail.com>
2015-03-31 18:24:32 -0700

    Sets up passport session

--- config/passport.js
+++ config/passport.js
@@ -1,32 +1,26 @@
-var passport = require('passport');
+/**
+ * Dependencies
+ */
 var LocalStrategy = require('passport-local').Strategy;
+var User = require('../models/User');
 
 /**
  * Passport Configuration
  */
-passport.use(new LocalStrategy(function(username, password, done){
-  Users.findOne({ username: username}, function(err, user){
-    if(err) { return done(err); }
-    if(!user){ 
-      return done(null, false, { message: 'Incorrect username.' });
-    }
-    hash(password, user.salt, function(err, hash){
-      if(err){return done(err);}
-      if(hash == user.hash){
-        return done(null, user);
-      }
-      done(null, false, {message: 'Incorrect password.'});
+var configuration = function(passport){
+  /**
+   * Session Setup
+   */
+  passport.serializeUser(function(user, done){
+    done(null, user.id);
+  });
+  passport.deserializeUser(function(id, done){
+    User.findById(id, function(err, user){
+      done(err, user);
     });
   });
-}));
-
-passport.serializeUser(function(user, done){
-  done(null, user.id);
-});
-passport.deserializeUser(function(id, done){
-  Users.findById(id, function(err, user){
-    if(err) done(err);
-    done(null, user);
-  })
-});
-
+  /**
+   * 
+   */
+}
+module.exports = configuration;
