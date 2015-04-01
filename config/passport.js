190e60523f23f14dd2a2e8fa586f3755e1280d22
Jimmy Gong <gong.jim@gmail.com>
2015-03-31 18:47:12 -0700

    Adds local strategy to passport config

--- config/passport.js
+++ config/passport.js
@@ -20,7 +20,38 @@ var configuration = function(passport){
     });
   });
   /**
-   * 
+   * Local Signup
    */
-}
+  passport.use('local-signup', new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password',
+    passReqToCallback: true
+  },
+  function(req, email, password, done){
+    process.nextTick(function(){
+      User.findOne({'local.email' : email}, function(err, user){
+        if(err){
+          return done(err);
+        }
+        if (user){
+          return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
+        }
+        else {
+          var newUser = new User();
+          /**
+           * Set local credentials
+           */
+          newUser.local.email = email;
+          newUser.local.password = newUser.generateHash(password);
+          newUser.save(function(err){
+            if(err){
+              throw err;
+            }
+            return done(null, newUser);
+          });
+        }
+      });
+    });
+  }));
+};
 module.exports = configuration;
