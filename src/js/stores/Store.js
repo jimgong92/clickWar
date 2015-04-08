7c183a939b8b6c693e566c927094212a593a3755
Jimmy Gong <gong.jim@gmail.com>
2015-04-08 00:01:47 -0700

    Scaffolds out some of app Store

---
+++ src/js/stores/Store.js
@@ -0,0 +1,8 @@
+var AppDispatcher = require('../dispatcher/AppDispatcher');
+var EventEmitter = require('events').EventEmitter;
+var Constants = require('../constants/Constants');
+var assign = require('object-assign');
+var $ = require('jquery');
+
+var CHANGE_EVENT = 'change';
+
