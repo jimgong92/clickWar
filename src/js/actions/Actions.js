44c353c757a296f18a0cc452f2355a26b4df4b89
Jimmy Gong <gong.jim@gmail.com>
2015-04-09 22:16:24 -0700

    Scaffolds out Actions and Store

---
+++ src/js/actions/Actions.js
@@ -0,0 +1,8 @@
+var AppDispatcher = require('../dispatcher/AppDispatcher');
+var Constants = require('../constants/TodoConstants');
+
+var Actions = {
+  
+};
+
+module.exports = Actions;
--- src/js/stores/Store.js
+++ src/js/stores/Store.js
@@ -6,3 +6,28 @@ var $ = require('jquery');
 
 var CHANGE_EVENT = 'change';
 
+var Store = assign({}, EventEmitter.prototype, {
+  emitChange: function(){
+    this.emit(CHANGE_EVENT);
+  },
+
+  addChangeListener: function(callback){
+    this.on(CHANGE_EVENT, callback);
+  },
+
+  removeChangeListener: function(callback){
+    this.removeListener(CHANGE_EVENT, callback);
+  }
+});
+
+/**
+ * Register callback to handle all updates
+ */
+AppDispatcher.register(function(action){
+  switch(action.actionType){
+    default: 
+      //no op
+  }
+};
+
+module.exports = Store;
